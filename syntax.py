44e5fe557d2911fa8cd64d498cc303d254f43666
pavanamjhi05-sys <pavanmajhi05@gmamil.com>
2026-04-16 16:57:03 +0530

    syntax & comments

---
+++ syntax.py
@@ -0,0 +1,9 @@
+#Basics Concept You have to Learn while code with Py
+# Python syntax
+print ("Hello pavan")
+
+# Comment In Python
+# using " # " You can write single line comments
+# For write multiple Lines of comment You Can simpily Write " " "    " " "  
+#example you can write like this... 
+"""  write your comment or documentetion Here.. """ 
